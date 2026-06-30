c08e66e8755133e6fcda3a2a7484a027fc1f3158
GaPortela <guilherme.portela4004@gmail.com>
2026-06-30 11:54:13 -0300

    Classe de Gereniciamento de Exceções criada e definidos métodos das Interfaces

--- Alunos/Guilherme Alves Portela/PROVA2BIM/IStorageService.java
+++ Alunos/Guilherme Alves Portela/PROVA2BIM/IStorageService.java
@@ -3,5 +3,17 @@
 */
 
 public interface IStorageService {
+    /**
+     * Salva todos os dados do usuário atual em um arquivo JSON.
+     * @param usuario O objeto usuário com suas respectivas listas.
+     * @throws ExceptionManager Se ocorrer um erro de escrita ou IO.
+     */
+    void salvarDados(Usuario usuario) throws ExceptionManager;
 
+    /**
+     * Carrega os dados do usuário a partir do arquivo JSON local.
+     * @return O objeto Usuario populado, ou null se o arquivo não existir.
+     * @throws ExceptionManager Se o arquivo estiver corrompido ou houver erro de leitura.
+     */
+    Usuario carregarDados() throws ExceptionManager;
 }
